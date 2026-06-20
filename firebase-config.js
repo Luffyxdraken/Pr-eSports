fd958e3d52a56aa6c767811e8ad32bc929ab39e2
Luffyxdraken <129548782+Luffyxdraken@users.noreply.github.com>
2026-06-21 03:04:46 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,9 @@
-// firebase-config.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Replace these values with your own Firebase project's config
 const firebaseConfig = {
   apiKey: "AIzaSyDpCqFgPRyyl_wEE9uNU50Ia9THW8ppq-k",
   authDomain: "chat-bot-ed471.firebaseapp.com",
@@ -17,12 +14,8 @@ const firebaseConfig = {
   measurementId: "G-N9YWE2NFY7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services
-const auth = getAuth(app);
-const db = getFirestore(app);
+export const auth = getAuth(app);
 
-// Export for use in other files
-export { app, auth, db };
+export const db = getFirestore(app);
